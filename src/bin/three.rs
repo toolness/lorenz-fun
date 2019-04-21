2ec53a88d25f09655f6f03accc36d379886e833e
Atul Varma <varmaa@gmail.com>
2019-04-21 10:07:24 -0400

    Provide two different Lorenz configurations.

--- src/bin/three.rs
+++ src/bin/three.rs
@@ -1,4 +1,5 @@
 use kiss3d::window::{Window, State};
+use kiss3d::event::{Action, WindowEvent, Key};
 use kiss3d::light::Light;
 use kiss3d::scene::SceneNode;
 use std::collections::vec_deque::VecDeque;
@@ -6,6 +7,11 @@ use nalgebra::{Vector3, UnitQuaternion, Translation3, Point3};
 
 const POINT_TRAIL_LEN: usize = 500;
 const POINT_TRAIL_INTENSITY_DECAY: f32 = 1.0 / POINT_TRAIL_LEN as f32;
+
+enum LorenzConfig {
+    One,
+    Two
+}
 
 struct Lorenz3d {
     lz: lorenz::Lorenz,
@@ -59,33 +65,81 @@ impl Lorenz3d {
             intensity -= POINT_TRAIL_INTENSITY_DECAY;
         }
     }
+
+    fn remove(&mut self, window: &mut Window) {
+        window.remove_node(&mut self.head);
+    }
 }
 
 struct AppState {
-    l3d: Lorenz3d
+    l3ds: Vec<Lorenz3d>
 }
 
 impl AppState {
-    fn new(window: &mut Window) -> Self {
-        let l3d = Lorenz3d::new(window)
-          .with_pos(0.1, 0.1, 0.1)
-          .with_color(1.0, 0.0, 0.0);
-        AppState { l3d }
+    fn new() -> Self {
+        AppState { l3ds: Vec::new() }
+    }
+
+    fn clear(&mut self, window: &mut Window) {
+        for l3d in self.l3ds.iter_mut() {
+            l3d.remove(window);
+        }
+        self.l3ds.clear();
+    }
+
+    fn init_config(&mut self, window: &mut Window, config: LorenzConfig) {
+        self.clear(window);
+        match config {
+            LorenzConfig::One => {
+                self.l3ds.push(Lorenz3d::new(window)
+                    .with_pos(0.1, 0.1, 0.1)
+                    .with_color(1.0, 1.0, 1.0));
+            },
+            LorenzConfig::Two => {
+                self.l3ds.push(Lorenz3d::new(window)
+                    .with_pos(0.1, 0.1, 0.1)
+                    .with_color(1.0, 0.0, 0.0));
+                self.l3ds.push(Lorenz3d::new(window)
+                    .with_pos(0.1000001, 0.1, 0.1)
+                    .with_color(0.0, 0.0, 1.0));
+            }
+        }
     }
 }
 
 impl State for AppState {
     fn step(&mut self, window: &mut Window) {
-        self.l3d.step(window);
+        for mut event in window.events().iter() {
+            match event.value {
+                WindowEvent::Key(button, Action::Release, _) => {
+                    match button {
+                        Key::Key1 => {
+                            self.init_config(window, LorenzConfig::One);
+                            event.inhibited = true;
+                        },
+                        Key::Key2 => {
+                            self.init_config(window, LorenzConfig::Two);
+                            event.inhibited = true;
+                        }
+                        _ => {}
+                    }
+                },
+                _ => {}
+            }
+        }
+        for l3d in self.l3ds.iter_mut() {
+            l3d.step(window);
+        }
     }
 }
 
 fn main() {
     let mut window = Window::new("lorenz-fun");
-    let app = AppState::new(&mut window);
+    let mut app = AppState::new();
 
     window.set_light(Light::StickToCamera);
     window.set_point_size(1.0);
+    app.init_config(&mut window, LorenzConfig::One);
 
     window.render_loop(app);
 }
